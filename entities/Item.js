cf406d91271c26c53cc4615514e9b5bb9fc6f0fb
Raito <minhquangmori214@gmail.com>
2025-07-21 21:26:28 +0700

    Increase item speed with level and add miss penalty

--- entities/Item.js
+++ entities/Item.js
@@ -1,13 +1,13 @@
 let idCounter = 1;
 
 export class Item {
-    constructor(x, y, width, height, score, image = null) {
+    constructor(x, y, width, height, score, image = null, speed = 200) {
         this.id = idCounter++;
         this.x = x;
         this.y = y;
         this.width = width !== null && width !== undefined ? width : (image ? image.width : 0);
         this.height = height !== null && height !== undefined ? height : (image ? image.height : 0);
-        this.speed = 200; // px/s
+        this.speed = speed; // px/s
         this.score = score;
         this.type = 'item';
         this.image = image;
--- systems/ItemSystem.js
+++ systems/ItemSystem.js
@@ -1,15 +1,18 @@
 import { Item } from '../entities/Item.js';
 
 export class ItemSystem {
-    constructor(canvas, scoreRef, itemImage = null) {
+    constructor(canvas, scoreRef, levelRef, itemImage = null) {
         this.canvas = canvas;
         this.spawnInterval = 1.0; // seconds
         this.timer = 0;
         this.scoreRef = scoreRef;
+        this.levelRef = levelRef;
         this.itemImage = itemImage;
     }
 
     update(entities, dt) {
+        const currentSpeed = 200 + (this.levelRef.value - 1) * 50;
+
         // Drop new items
         this.timer += dt;
         if (this.timer >= this.spawnInterval) {
@@ -18,11 +21,12 @@ export class ItemSystem {
             const x = Math.random() * (this.canvas.width - imgW);
             const score = 10;
             const imgH = this.itemImage ? this.itemImage.height : 30;
-            entities.push(new Item(x, 0, imgW, imgH, score, this.itemImage));
+            entities.push(new Item(x, 0, imgW, imgH, score, this.itemImage, currentSpeed));
         }
 
-        // Move items down
+        // Move items down and apply speed for current level
         entities.filter(e => e.type === 'item').forEach(item => {
+            item.speed = currentSpeed;
             item.y += item.speed * dt;
         });
     }
